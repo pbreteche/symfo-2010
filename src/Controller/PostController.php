eb69ee7bd766f1014a8712630cc32d036f1f4de1
Pierre Bretéché <pbreteche@dawan.fr>
2020-11-04 11:41:19 +0100

    Use repository to fetch data and display in twig

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class PostController extends AbstractController
     /**
      * @Route("/post", methods="GET")
      */
-    public function index(): Response
+    public function index(PostRepository $repository): Response
     {
+        $posts = $repository->findAll();
+
         return $this->render('post/index.html.twig', [
-            'twig_variable_name' => 'ma valeur',
+            'posts' => $posts,
         ]);
     }
 }
