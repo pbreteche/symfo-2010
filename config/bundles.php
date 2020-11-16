3eabb71840c2bf6a8dc9fe5c17bdf126b8939b23
Pierre Bretéché <pbreteche@dawan.fr>
2020-11-16 11:05:15 +0100

    composer require security

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
