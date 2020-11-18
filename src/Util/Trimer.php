5d16f57451da76cf4b14e530fd0fc22ed9519461
Pierre Bretéché <pbreteche@dawan.fr>
2020-11-18 14:23:21 +0100

    primitive constructor parameter

--- src/Util/Trimer.php
+++ src/Util/Trimer.php
@@ -4,8 +4,16 @@ namespace App\Util;
 
 class Trimer
 {
+
+    private $charList;
+
+    public function __construct(string $charList)
+    {
+        $this->charList = $charList;
+    }
+
     public function trim(string $string)
     {
-        return trim($string);
+        return trim($string, $this->charList);
     }
 }
